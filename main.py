1142c617574fba39d6eb25e1c1e3acedd71679b4
SMcDowell <160181671+Luicazen@users.noreply.github.com>
2024-09-10 14:16:46 -0600

    Update main.py
    fix return mispell in feet_to_meters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def feet_to_meters(value):
-    retun value * 0.3048
-
+def meters_to_feet(value):
+    return value / 0.3048
 feet = float(input('? '))
 print(f'{feet}ft = {feet_to_meters(feet)}mt')
+def meters_to_feet(value):
+    return value / 0.3048
